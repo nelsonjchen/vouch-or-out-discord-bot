b74fdad9ee18001b7c90eb58727b40935b3233f6
Nelson Chen <crazysim@gmail.com>
2023-08-12 19:25:09 -0700

    initial commit

---
+++ src/commands.ts
@@ -0,0 +1,9 @@
+/**
+ * Share command metadata from a common spot to be used for both runtime
+ * and registration.
+ */
+
+export const VOUCH_COMMAND = {
+	name: 'vouch',
+	description: 'Vouch for a user',
+};
