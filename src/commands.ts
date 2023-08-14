5911af40e92f4ebf88c06696a3817a3b807ce23b
Nelson Chen <crazysim@gmail.com>
2023-08-13 23:30:48 -0700

    Add commands

--- src/commands.ts
+++ src/commands.ts
@@ -6,4 +6,18 @@
 export const VOUCH_COMMAND = {
 	name: 'vouch',
 	description: 'Vouch for a user',
+	"options": [
+		{
+			name: 'user',
+			description: 'User to vouch for',
+			type: 6,
+			required: 'true'
+		},
+		{
+			name: 'reason',
+			description: 'Reason for vouching',
+			type: 3,
+			required: 'true'
+		}
+	]
 };
