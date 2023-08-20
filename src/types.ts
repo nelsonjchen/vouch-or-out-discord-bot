6656868e5fb86fa2382d84eea4f82315ebe41967
Nelson Chen <crazysim@gmail.com>
2023-08-19 23:14:29 -0700

    Add a type for vouch command interaction

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,13 @@
+import { MessageComponentTypes } from "discord-interactions";
+
+
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
 	// MY_KV_NAMESPACE: KVNamespace
 	//
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
 	// MY_DURABLE_OBJECT: DurableObjectNamespace
-  Vouch: DurableObjectNamespace
+	Vouch: DurableObjectNamespace
 	//
 	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
 	// MY_BUCKET: R2Bucket
@@ -14,7 +17,7 @@ export interface Env {
 	//
 	DISCORD_APPLICATION_ID: string;
 	//
-	
+
 	SKIP_DISCORD_VALIDATION: string;
 }
 
@@ -22,4 +25,76 @@ export interface Env {
 export interface UserVouch {
 	userId: string
 	reason: string
+}
+
+
+// Example incoming interaction type
+// E.g.
+// {
+// 	"type": 2,
+// 	"token": "A_UNIQUE_TOKEN",
+// 	"member": {
+// 			"user": {
+// 					"id": "53908232506183680",
+// 					"username": "Mason",
+// 					"avatar": "a_d5efa99b3eeaa7dd43acca82f5692432",
+// 					"discriminator": "1337",
+// 					"public_flags": 131141
+// 			},
+// 			"roles": ["539082325061836999"],
+// 			"premium_since": null,
+// 			"permissions": "2147483647",
+// 			"pending": false,
+// 			"nick": null,
+// 			"mute": false,
+// 			"joined_at": "2017-03-13T19:19:14.040000+00:00",
+// 			"is_pending": false,
+// 			"deaf": false
+// 	},
+// 	"id": "786008729715212338",
+// 	"guild_id": "290926798626357999",
+// 	"app_permissions": "442368",
+// 	"guild_locale": "en-US",
+// 	"locale": "en-US",
+// 	"data": {
+// 			"options": [{
+// 					"type": 3,
+// 					"name": "cardname",
+// 					"value": "The Gitrog Monster"
+// 			}],
+// 			"type": 1,
+// 			"name": "cardsearch",
+// 			"id": "771825006014889984"
+// 	},
+// 	"channel_id": "645027906669510667"
+// }
+
+export interface VouchCommandInteraction {
+	type: 2;
+	member: {
+		user: {
+			id: string;
+			username: string;
+		}
+		roles: string[];
+	},
+	id: string;
+	data: {
+		options: [
+			{
+				type: MessageComponentTypes.USER_SELECT;
+				name: 'user';
+				value: string;
+			},
+			{
+				type: MessageComponentTypes.INPUT_TEXT;
+				name: 'reason';
+				value: string;
+			}
+		],
+		type: 1;
+		name: 'vouch';
+		id: string;
+	},
+	channel_id: string;
 }
